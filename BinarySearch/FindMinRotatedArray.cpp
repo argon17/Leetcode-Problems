d87f2fcdacc15d96dcb4689d9ce673e884911b22
argon <premsagark.ug18.ee@nitp.ac.in>
2021-09-15 14:42:57 +0530

    binary search

--- BinarySearch/FindMinRotatedArray.cpp
+++ BinarySearch/FindMinRotatedArray.cpp
@@ -1,3 +1,5 @@
+// if consisting of Unique elements
+
 class Solution {
 public:
     int findMin(vector<int>& nums) {
@@ -6,6 +8,24 @@ public:
             int mid = (lo + hi) >> 1;
             if(nums[mid] < nums[hi]) hi = mid;
             else lo = mid + 1;
+        }
+        return nums[lo];
+    }
+};
+
+// if consisting of Duplicates elements
+
+class Solution {
+public:
+    int findMin(vector<int>& nums) {
+        int n = nums.size();
+        int lo = 0, hi = n - 1;
+        while(lo < hi){
+            int mid = (lo + hi) >> 1;
+            // only eliminate the elements, you're sure of
+            if(nums[mid] < nums[hi]) hi = mid;
+            else if(nums[mid] > nums[hi]) lo = mid + 1;
+            else --hi;
         }
         return nums[lo];
     }
