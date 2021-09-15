32d3a141cd34c1f539eda6604bbbf037fbf3a63f
argon <premsagark.ug18.ee@nitp.ac.in>
2021-09-15 19:28:05 +0530

    dp and map problems

---
+++ Geometry/MaxPointsOnALine.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int maxPoints(vector<vector<int>>& points) {
+        int n = points.size();
+        if ( n <= 2 ) return n;
+        int ans = 2;
+        for ( int i = 0; i < n; i++ ) {
+            unordered_map<double, int> mp;
+            for ( int j = i+1; j < n; j++ ) {
+                double a = points[j][0] - points[i][0], b = points[j][1] - points[i][1];
+                if ( b != 0 ) mp[a / b]++;
+                else mp[(double)INT_MAX]++;
+            }
+            for ( auto i : mp ) ans = max(ans, 1 + i.second);
+        }
+        return ans;
+    }
+};
