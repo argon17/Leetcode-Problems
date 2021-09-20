8c5dc38d653528712622a2ecf995de6839005766
argon <premsagark.ug18.ee@nitp.ac.in>
2021-09-20 11:22:52 +0530

    sliding window problems

--- SlidingWindow/twoPointersSubarrayProblems.cpp
+++ SlidingWindow/twoPointersSubarrayProblems.cpp
@@ -1,16 +1,16 @@
+// O(2*N) = O(N) TC and O(1) SC
+// worst case : l and r both reach upto N 
+
 class Solution
 {
 public:
     int numSubarrayProductLessThanK(vector<int> &nums, int k)
     {
         int l = 0, r = 0;
-        int p = 1;
-        int ans = 0;
-        while (r < nums.size())
-        {
+        int p = 1, ans = 0;
+        while (r < nums.size()) {
             p *= nums[r];
-            while (l <= r && p >= k)
-                p /= nums[l++];
+            while (l <= r && p >= k) p /= nums[l++];
             ans += r++ - l + 1;
         }
         return ans;
